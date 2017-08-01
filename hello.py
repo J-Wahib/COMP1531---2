9375664cbb981c82e9e8eb7ae2e9dcb73762a119
z5164984 <z5164984@cse.unsw.edu.au>
2017-08-01 11:39:30 +1000

    hundred fizzes

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
 print("hello world")
+
+print("Chicken McNoogets")
+
+def fizzbuzz ():
+    n = 0
+    for n in range(101):     
+        if n%5 == 0 & n%3 == 0:
+            print("Fizz Buzz")
+        elif n%3 == 0:
+	        print("Fizz")
+        elif n%5 == 0:
+	        print("Buzz")
+        
+        else:
+	        print(n)
+        
+    
+        
+
+
+fizzbuzz()
