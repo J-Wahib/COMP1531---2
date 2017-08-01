aa4611eec1711fff23d1843ffded3ffa090ee3ef
z5164984 <z5164984@cse.unsw.edu.au>
2017-08-01 11:31:51 +1000

    fizzbuzz

--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
 print("hello world")
 print("Chicken McNoogets")
+
+def fizzbuzz ():
+    n = 6
+    while not n < 0:
+        n = int(input("Giff noombars: "))
+        if n%3 == 0:
+	        print("Fizz")
+        elif n%5 == 0:
+	        print("Buzz")
+        else:
+	        print(n)
+        
+    
+        
+
+
+fizzbuzz()
